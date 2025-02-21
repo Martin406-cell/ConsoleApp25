1ee16123d27b7c4fbbbe9274f005fd0da6463934
MEGA <MEGA@gmail.com>
2025-02-21 17:11:31 +0200

    Add project files.

---
+++ ConsoleApp25/Worker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Worker(string name, double salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Salary}";
+        }
+    }
+}
+    
+
